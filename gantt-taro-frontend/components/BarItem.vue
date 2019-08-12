f81521a61266c3ee7ac3e0702052db093f6e64c3
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2019-08-12 14:33:40 +0900

    wip

--- gantt-taro-frontend/components/BarItem.vue
+++ gantt-taro-frontend/components/BarItem.vue
@@ -1,22 +1,14 @@
 <template>
-<div class="bar"
-    :style="{ 'margin-left': margin_left + 'px' }"
-    @mousemove="mouse_move"
-    >
-        <div class="bar-front mouse-left"
-            @mousedown="mouse_down($event, 'left')"
-            @mouseup="mouse_up($event, 'left')"
-        ></div>
-        <div class="bar-middle mouse-middle"
-            :style="{ 'width': width + 'px' }"
-            @mousedown="mouse_down($event, 'middle')"
-            @mouseup="mouse_up($event, 'middle')"
-        ></div>
-        <div class="bar-end mouse-right"
-            @mousedown="mouse_down($event, 'right')"
-            @mouseup="mouse_up($event, 'right')"
-        ></div>
-    </div>
+<v-progress-linear
+    height="30"
+    :buffer-value="width"
+    :value="width / 10"
+    :style="{ 'margin-left': margin_left + 'px', cursor: cursor }"
+    @mousemove="change_cursor"
+    @mousedown="mouse_down"
+    @mouseup="mouse_up"
+>
+</v-progress-linear>
 </template>
 
 <script>
@@ -28,31 +20,52 @@ export default {
             start_right: 0,
             current_target: null,
 
-            width: 30,
+            width: 10,
+            max_width_px: 0,
             margin_left: 0,
+            cursor: "",
         }
     },
     methods: {
-        mouse_down(e, v) {
+        is_target(e) {
+            return e.target.className === "v-progress-linear__buffer"
+        },
+        mouse_down(e) {
+            if (!this.is_target(e)) {
+                return
+            }
             this.can_move = true
             this.start_right = e.clientX
             this.start_left = e.clientX
-            this.current_target = v
+
+            this.max_width_px = e.target.clientWidth / this.width * 100
         },
         mouse_up() {
             this.can_move = false
         },
+        change_cursor(e) {
+            if (this.can_move) {
+                this.mouse_move(e)
+                return
+            }
+
+            if (!this.is_target(e)) {
+                this.cursor = ""
+                return
+            }
+
+            if (e.target.clientWidth - e.offsetX <= 10) {
+                this.cursor = "e-resize"
+                this.current_target = "right"
+            } else {
+                this.cursor = "move"
+                this.current_target = "middle"
+            }
+        },
         mouse_move(e) {
             if (this.can_move) {
                 if (this.current_target === "right") {
-                    const len = e.clientX - this.start_right
-                    this.width += len
-                    this.start_right = e.clientX
-                } else if (this.current_target === "left") {
-                    const len = e.clientX - this.start_left
-                    this.width -= len
-                    this.margin_left += len
-                    this.start_left = e.clientX
+                    this.width = (e.target.clientWidth + e.movementX) / this.max_width_px * 100
                 } else if (this.current_target === "middle") {
                     const len = e.clientX - this.start_left
                     this.margin_left += len
@@ -68,26 +81,5 @@ export default {
 .bar {
     height: 30px;
     font-size: 0;
-}
-.bar-front, .bar-end {
-    height: 100%;
-    width: 10px;
-    background-color: grey;
-    display: inline-block;
-}
-.bar-middle {
-    height: 100%;
-    background-color: steelblue;
-    display: inline-block;
-}
-
-.mouse-right {
-    cursor: e-resize	;
-}
-.mouse-middle {
-    cursor: move;
-}
-.mouse-left {
-    cursor: w-resize	;
 }
 </style>
