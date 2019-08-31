8665a0e600db8c41a759d317570f04728fd1ed5d
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2019-09-01 00:45:19 +0900

    add get tasks by api server

--- gantt-taro-frontend/components/GanttChart.vue
+++ gantt-taro-frontend/components/GanttChart.vue
@@ -4,6 +4,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import Gantt from "frappe-gantt";
 
 export default {
@@ -15,57 +16,33 @@ export default {
   data() {
     return {
       gantt: null,
-      tasks: [
-        {
-          id: 'task_a',
-          start: '2019-07-31',
-          end: '2019-08-09',
-          name: 'task_a',
-          progress: 0
-        },
-        {
-          id: 'task_b',
-          start: '2019-08-05',
-          end: '2019-08-23',
-          name: 'task_b',
-          progress: 0,
-          dependencies: 'task_a',
-        },
-        {
-          id: 'task_c',
-          start: '2019-08-11',
-          end: '2019-08-15',
-          name: 'task_c',
-          progress: 0,
-          dependencies: 'task_b',
-        },
-        {
-          id: 'task_d',
-          start: '2019-08-11',
-          end: '2019-08-15',
-          name: 'task_d',
-          progress: 0,
-          dependencies: 'task_a',
-        },
-        {
-          id: 'task_e',
-          start: '2019-08-15',
-          end: '2019-08-18',
-          name: 'こんにちはする',
-          progress: 0,
-          dependencies: 'task_c',
-        }
-      ]
+      tasks: []
     }
   },
-  mounted() {
+  async mounted() {
+    await this.get_all_tasks()
     this.create()
-
-    console.log(this.gantt)
 
     this.scroll_today()
   },
   methods: {
+    async get_all_tasks() {
+      const res = await axios.get("/api/v1/tasks", {
+        headers: {
+          "Authorization": `${this.$store.getters.token}`
+        }
+      })
+
+      this.tasks = res.data.map(v => {
+        return {
+          id: `task_${v.id}`,
+          start: v.started_at,
+          end: v.ended_at,
+          name: v.name,
+          progress: 0,
+        }
+      })
+    },
     create() {
       const f = this.tasks.every(o => o.id !== "task_new")
       if (f) {
